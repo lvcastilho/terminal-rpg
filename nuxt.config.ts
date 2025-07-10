d08e6ee092135cf7d5779022aa937e3b10bf1147
LucasCastilho7 <lucas@saudeagora.com.vc>
2025-07-10 19:03:36 -0300

    init new modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint'
+  ]
 })
